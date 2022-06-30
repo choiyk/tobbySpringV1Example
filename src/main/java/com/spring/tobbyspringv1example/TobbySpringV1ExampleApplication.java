edb7289eb0a075c5621455e31858d69d76476285
choiyk <d_pvmf@naver.com>
2022-06-30 20:47:24 +0900

    init project

---
+++ src/main/java/com/spring/tobbyspringv1example/TobbySpringV1ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.tobbyspringv1example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobbySpringV1ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobbySpringV1ExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/tobbyspringv1example/TobbySpringV1ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.tobbyspringv1example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobbySpringV1ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
