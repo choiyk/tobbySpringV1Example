dc00ee21dd5021170bfff73a131c3684a6c57f3c
choiyk <d_pvmf@naver.com>
2022-07-10 18:50:08 +0900

    DataSource 인터페이스 적용

--- src/main/java/com/spring/tobbyspringv1example/doamin/UserDao.java
+++ src/main/java/com/spring/tobbyspringv1example/doamin/UserDao.java
@@ -5,6 +5,7 @@ import com.spring.tobbyspringv1example.dao.DaoFactory;
 import com.spring.tobbyspringv1example.dao.SimpleConnectionMaker;
 import com.spring.tobbyspringv1example.dao.User;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
@@ -13,6 +14,7 @@ public class UserDao {
     자신이 사용하는 다른 싱글톤 빈을 저장하려는 용도라면 인스턴스 변수를 사용해도 좋다.
      */
     private ConnectionMaker connectionMaker;
+    private DataSource dataSource;
 
     /* 생성자를 이용한 의존관계 주입
     public UserDao(ConnectionMaker connectionMaker) {
@@ -24,8 +26,13 @@ public class UserDao {
         this.connectionMaker = connectionMaker;
     }
 
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        //Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
@@ -40,7 +47,8 @@ public class UserDao {
 
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        //Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
         PreparedStatement ps = c
                 .prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
