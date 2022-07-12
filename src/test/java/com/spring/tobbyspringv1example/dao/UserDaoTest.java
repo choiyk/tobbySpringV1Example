de82986619f0aa42a778052f528f427dadb0de11
choiyk <d_pvmf@naver.com>
2022-07-12 19:14:32 +0900

    테스트 결과의 일관성

--- src/test/java/com/spring/tobbyspringv1example/dao/UserDaoTest.java
+++ src/test/java/com/spring/tobbyspringv1example/dao/UserDaoTest.java
@@ -1,14 +1,19 @@
 package com.spring.tobbyspringv1example.dao;
 
 import com.spring.tobbyspringv1example.doamin.UserDao;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserDaoTest {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+    @Test
+    public void addAndGet() throws ClassNotFoundException, SQLException {
         /*
         애플리케이션 컨텍스트 : 빈 팩토리를 확장한 IoC 컨테이너.(빈 팩토리 + 스프링이 제공하는 부가 서비스)
         빈 팩토리 : 스프링의 IoC를 담당하는 핵심 컨테이너. 빈 등록, 생성, 조회, 제공 등 빈을 관리.
@@ -17,25 +22,20 @@ public class UserDaoTest {
         ApplicationContext context = new GenericXmlApplicationContext("ApplicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
+        dao.deleteAll();
+        assertEquals(dao.getCount(), 0);
+
         User user = new User();
         user.setId("whiteship");
         user.setName("백기선");
         user.setPassword("married");
 
         dao.add(user);
-
-        System.out.println(user.getId() + " 등록 성공");
+        assertEquals(dao.getCount(), 1);
 
         User user2 = dao.get(user.getId());
         
-        if(!user.getName().equals(user2.getName())){
-            System.out.println("테스트 실패 (name)");
-        }
-        else if(!user.getPassword().equals(user2.getPassword())) {
-            System.out.println("테스트 실패 (password)");
-        }
-        else {
-            System.out.println("조회 테스트 성공");
-        }
+        assertEquals(user2.getName(), user.getName());
+        assertEquals(user2.getPassword(), user.getPassword());
     }
 }
