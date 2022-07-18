1c0ba1e918c1575af33a384bb2a412075828d74e
choiyk <d_pvmf@naver.com>
2022-07-18 18:17:06 +0900

    코드를 이용한 수동 DI
    - 장점 : 굳이 인터페이스를 두지 않아도 될 만큼 긴밀한 관계를 갖는 DAO 클래스와 JdbcContext를 어색하게 따로 빈으로 분리하지 않고 내부에서 직접 만들어 사용하면서도 다른 오브젝트에 대한 DI를 적용할 수 있다. 또, 그 관계를 외부에는 감출 수 있다.
    - 단점 : JdbcContext를 여러 오브젝트가 사용하더라도 싱글톤으로 만들 수 없고, DI 작업을 위한 부가적인 코드가 필요하다.

--- src/main/java/com/spring/tobbyspringv1example/doamin/UserDao.java
+++ src/main/java/com/spring/tobbyspringv1example/doamin/UserDao.java
@@ -21,10 +21,11 @@ public class UserDao {
      */
     //수정자 메소드를 이용한 의존관계 주입
     public void setDataSource(DataSource dataSource) {
+        this.jdbcContext = new JdbcContext();
+        this.jdbcContext.setDataSource(dataSource);
+
         this.dataSource = dataSource;
     }
-
-    public void setJdbcContext(JdbcContext jdbcContext) {this.jdbcContext = jdbcContext;}
 
     public void add(final User user) throws SQLException {
         StatementStrategy st = new StatementStrategy() {
